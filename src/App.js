92eb8790cfcad3de332684c50d7ef42fa813906c
Alamin <60449971+alaminprogramerr@users.noreply.github.com>
2020-03-31 11:27:20 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
 import { useState } from 'react';
-
 import getYouTubeID from 'get-youtube-id'
+import './app.css'
+import convert  from'convert-seconds'
+import RestoreIcon from '@material-ui/icons/Restore';
+import LibraryAddCheckRoundedIcon from '@material-ui/icons/LibraryAddCheckRounded';
+
 
 const useStyles = makeStyles({
   root: {
@@ -19,22 +23,33 @@ function valuetext(value) {
 export default function RangeSlider() {
   const classes = useStyles();
   const [value, setValue] = React.useState([0, 100]);
-  const [link, setlink] = useState('https://www.youtube.com/embed/MqNd1xD46VY')
+  const [avarageTime, setavarageTime] = useState('')
+  const [selected, setselected] = useState('00:00')
+
+
 
   const handleChange = (event, newValue) => {
     var video_duration = localStorage.getItem('video_duration')
-    // console.log(event.target.value)
     var onePersentInSeconds=video_duration/100
-    console.log(newValue)
-    // newValue=[1, 100]
     var valueOne=newValue[0]*onePersentInSeconds
     var valueTwo=newValue[1]*onePersentInSeconds
+
+    var selectedMinute=Math.floor((valueTwo-valueOne) / 60);
+    var abc=((valueTwo-valueOne)-selectedMinute*60).toString()
+    var selectedSeconds=abc.split('.')[0]
     setValue(newValue);
+    setselected(`${selectedMinute}:${selectedSeconds}`)
+
+
     
+    
+    // here just need to set    selected time  ,  avarage time 
     window.localStorage.setItem('valueOne',valueOne)
     window.localStorage.setItem('valueTwo',valueTwo)
     window.localStorage.setItem('update','commit')
+
     
+    setavarageTime()
     console.log(valueOne, valueTwo)
     console.log(localStorage.getItem('update'))
     
@@ -46,6 +61,15 @@ export default function RangeSlider() {
     window.localStorage.setItem('update','commit')
 
   })
+  useEffect(()=>{
+    setTimeout(() => {
+      var video_duration = localStorage.getItem('video_duration')
+      var minute=Math.floor(video_duration / 60);
+      var seconds=video_duration-(minute*60)
+
+      setavarageTime(`${minute} : ${seconds}`)
+    }, 200);
+  })
   const changeHandler=(event)=>{
     var videoID=getYouTubeID(event.target.value)
     window.localStorage.setItem('getVideoID',`${videoID}`)
@@ -53,7 +77,6 @@ export default function RangeSlider() {
   }
   return (
     <div >
-      
       <div >
         <div>
           <div className="container">
@@ -67,14 +90,26 @@ export default function RangeSlider() {
                         <div className="col-md-12  d-flex">
                           <input onChange={changeHandler} className="form-control  mb-5" type="text" placeholder="Enter a youtube video link " />
                         </div>
-                        {/* <div style={{textAlign: 'center'}}>
-                          <div style={{display:'inline-block'}}>
-                            <iframe title="YouTube Video" id="video_content" width="560" height="315" src={`${link}?&start=${value[0]}&end=${value[1]}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                          <div className="col-md-12  ">
+                            <div style={{padding:'164px'}}>
+                                <p className="text-center" style={{whiteSpace:'nowrap'}}>Loading ...</p>
+                            </div>
                           </div>
-                        </div> */}
                       </div>
-                      <div class="card-action">
-                        <Typography id="range-slider" gutterBottom>Select Range </Typography>
+                      <div className="row">
+                        <div style={{minWidth:'100px'}} className=" text-center">
+                          <p><RestoreIcon /></p>
+                          <p className="text-center " style={{whiteSpace:'nowrap'}}> {avarageTime} </p>
+                        </div>
+                        <div style={{minWidth:'100px'}} className=" text-center">
+                          <p><LibraryAddCheckRoundedIcon /></p>
+                          <p className="text-center " style={{whiteSpace:'nowrap'}}> {selected} </p>
+                        </div>
+                      </div>
+                      <div className="card-action">
+                        <div>
+                          <Typography id="range-slider" gutterBottom>Select Range </Typography>
+                        </div>
                         <Slider
                         value={value}
                           onChange={handleChange}
