89b82500b3cd660a0c0af41945c601360a76496b
Alamin <60449971+alaminprogramerr@users.noreply.github.com>
2020-03-31 00:24:39 -0500

    need to input url just

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
 import { useState } from 'react';
 
+import getYouTubeID from 'get-youtube-id'
+
 const useStyles = makeStyles({
   root: {
     width: 300,
@@ -18,48 +20,64 @@ export default function RangeSlider() {
   const classes = useStyles();
   const [value, setValue] = React.useState([0, 100]);
   const [link, setlink] = useState('https://www.youtube.com/embed/MqNd1xD46VY')
+  const [videoID, setVideoID] = useState('')
 
   const handleChange = (event, newValue) => {
-    console.log(event.target.value)
+    var video_duration = localStorage.getItem('video_duration')
+    // console.log(event.target.value)
+    var onePersentInSeconds=video_duration/100
     console.log(newValue)
+    // newValue=[1, 100]
+    var valueOne=newValue[0]*onePersentInSeconds
+    var valueTwo=newValue[1]*onePersentInSeconds
     setValue(newValue);
+    
+    window.localStorage.setItem('valueOne',valueOne)
+    window.localStorage.setItem('valueTwo',valueTwo)
+    window.localStorage.setItem('update','commit')
+    console.log(valueOne, valueTwo)
+    console.log(localStorage.getItem('update'))
+    
   };
-
-  // useEffect(()=>{
-  //   document.getElementById
-  // })
+  useState(()=>{
+    
+    window.localStorage.setItem('valueOne',0)
+    window.localStorage.setItem('valueTwo',100)
+    window.localStorage.setItem('update','commit')
+  })
+  const show=(event)=>{
+    console.log(localStorage.getItem('video_duration'))
+  }
   const changeHandler=(event)=>{
     setlink(event.target.value)
+    setVideoID(getYouTubeID(event.target.value))
+    setTimeout(() => {
+      console.log(videoID)
+    }, 2000);
   }
   return (
     <div >
       
       <div >
         <div>
-        <nav>
-          <div class="nav-wrapper">
-            <div class="container">
-              <ul id="nav-mobile" class="text-center hide-on-med-and-down">
-                <li><a href="" class="text-center">Video Play</a></li>
-              </ul>
-            </div>
-          </div>
-        </nav>
           <div className="container">
-            <div className=" mt-5">
+          <div id="video-placeholder"></div>
+
+            <div className="">
+              {console.log(videoID)}
               <div class="row">
-                <div class="col-md-10 offset-md-1">
+                <div class="col-md-12 offset-md-0">
                   <div class="card">
-                      <div class="card-content">
+                      {/* <div class="card-content">
                         <div className="col-md-10 offset-md-1 d-flex">
                           <input onChange={changeHandler} className="form-control  mb-5" type="text" placeholder="Enter a link " />
                         </div>
                         <div style={{textAlign: 'center'}}>
                           <div style={{display:'inline-block'}}>
-                            <iframe title="YouTube Video" id="video_content" width="560" height="315" src={`${link}?&start=${value[0]}&end=${value[1]}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <iframe title="YouTube Video" id="video_content" width="560" height="315" src={`${link}?&start=${value[0]}&end=${value[1]}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
                           </div>
                         </div>
-                      </div>
+                      </div> */}
                       <div class="card-action">
                         <Typography id="range-slider" gutterBottom>Select Range </Typography>
                         <Slider
@@ -70,6 +88,7 @@ export default function RangeSlider() {
                           getAriaValueText={valuetext}
                         />
                       </div>
+                      <button onClick={show} className="btn">show</button>
                   </div>
                 </div>
               </div>
